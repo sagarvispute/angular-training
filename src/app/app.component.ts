8a82e64ea727c3b5fbe7e3273596e42df8f2fb32
sagar vispute <sagarvispute33@gmail.com>
2020-07-04 21:12:16 +0530

    Session 1 and 2 data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practice';
+
+  name: string = 'sagar';
+  contact: number = 45484845;
+  active: boolean = true;
+  subjects: string[] = ['Hindi', 'English', 'Marathi'];
+
+  user: any = {
+    name: 'sagar',
+    contact: 78454841,
+    online: true
+  };
+
+  users: any[] = [
+    {
+      name: 'sagar',
+      contact: 78454841,
+      online: true
+    },
+    {
+      name: 'Santosh',
+      contact: 78454841,
+      online: true
+    },
+    {
+      name: 'Sameer',
+      contact: 78454841,
+      online: true
+    },
+    {
+      name: 'Ashwini',
+      contact: 78454841,
+      online: true
+    },
+    {
+      name: 'Rupa',
+      contact: 78454841,
+      online: true
+    }
+  ];
 }
